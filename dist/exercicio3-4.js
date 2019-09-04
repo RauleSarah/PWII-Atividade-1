ef1c68ff8d672adddcb0c7307b0f72e5c098bdb0
Sarah e Raul <raulglitzk26@gmail.com>
2019-09-04 20:02:14 -0300

    Exercicio 3

---
+++ dist/exercicio3-4.js
@@ -0,0 +1,7 @@
+var n = 8;
+if (n % 2 == 0) {
+    console.log("O número é par");
+}
+else {
+    console.log("O número é impar");
+}
---
+++ exercicio3-4.ts
@@ -0,0 +1,8 @@
+let n: number = 8; 
+
+if (n %2 == 0){
+    console.log("O número é par")
+}
+else {
+    console.log("O número é impar" )
+}
