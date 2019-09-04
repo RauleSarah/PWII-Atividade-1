c2436a5d4bb578813fbc487b9a9c72cd2c265d55
Sarah e Raul <raulglitzk26@gmail.com>
2019-09-04 20:34:17 -0300

    Exercicio 5

---
+++ dist/exercicio5.js
@@ -0,0 +1,4 @@
+var comprimento = 2;
+var largura = 3;
+var farea = function (comprimento, largura) { return (comprimento * largura); };
+console.log("A \u00E1rea \u00E9 " + farea(comprimento, largura));
---
+++ exercicio5.ts
@@ -0,0 +1,5 @@
+let comprimento = 2
+let largura = 3
+
+let farea= (comprimento:number,largura:number) => (comprimento * largura )
+console.log(`A área é ${farea(comprimento,largura)}`)
